621f46f26c4d7ed1a406d35327c03f58ab01106f
Geosito <132366917+Geosito@users.noreply.github.com>
2023-06-30 21:51:45 -0300

    Add files via upload

---
+++ Documentacion/pruebas/javascp.js
@@ -0,0 +1,12 @@
+function showMessage() {
+    var message = document.getElementById("message");
+    message.style.display = "block";
+  }
+  
+
+
+  function showProduct(productId) {
+    var product = document.getElementById(productId);
+    product.style.display = "block";
+  }
+  
